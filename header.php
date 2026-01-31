579238f41620c89d811e7421ec3b67758cfe4204
xudhidvxidgxn <ys2510103@y-y.hs.kr>
2026-01-31 12:38:10 +0900

    이우진

---
+++ header.php
@@ -0,0 +1,21 @@
+<style>
+    header {
+        display: flex;
+        width: 100%;
+        height: 60px;
+        border-bottom: 1px solid #ddd;
+        justify-content: space-between;
+    }
+    header nav ul {
+        display: flex;
+    }
+</style>
+<header>
+    <div class="logo"></div>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+    </head>
+    <body>
+        <?php 
+        // php: 동적 컨텡츠를 만드는 언어다
+        // php는 브라우저에서 읽을 수 없다. => 즉, 서버가 필요하다.
+        // php 코드를 어떻게 해석하지? => php 인터프리터    
+        // 인터프리터가 한 줄 한 줄 읽어들인다. (해석한다.)
+
+        // php에서 자주 사용하는 함수: require_once, foreach
+         
+        // 이 파일을 불러오는 함수(키워드)
+            require_once "header.php";
+        ?>
+        
+        <!-- $db -->
+        index.html
+        <a href="https://naver.com">naver.com</a>
+    </body>
+</html>
